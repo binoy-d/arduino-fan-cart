ffc7f9157a5b29581b50715184bc40b2e6cbcb48
Daniel Binoy <dbinoy15@gmail.com>
2018-03-12 16:31:11 -0700

    Wrote Pseudocode
    Wrote up the pseudocode.
    It reads the speed and adjust the motorspeed to error correct a little in each loop

--- fancart/fancart.ino
+++ fancart/fancart.ino
@@ -1,11 +1,12 @@
-
-
+//desiredspeed = ?
+//motorspeed = desiredspeed
 void setup() {
-  
-
+  //set up pins for motor and accelerometer
 }
 
 void loop() {
-  
-
+  //currentspeed = read(accelerometer)
+  //if currentspeed < desiredspeed, motorspeed ++
+  //else,  motorspeed --
+  //set motor's speed to motorspeed
 }
