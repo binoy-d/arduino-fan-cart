86e75bf689558b8a0f934a22a50acf23fbf03ac8
Daniel Binoy <bleeblob43@gmail.com>
2018-03-14 16:10:28 -0700

    Update fancart.ino

--- fancart/fancart.ino
+++ fancart/fancart.ino
@@ -1,8 +1,18 @@
+#include <AFMotor.h>
+
+
 int desiredSpeed = 10;// m/s
 int motorSpeed = desiredSpeed;//not m/s.
 
+
+// Use the connections for motor number 4 on the Arduino v1.0 motor shield.
+AF_DCMotor motor(4);
+
 void setup() {
-  //set up pins for motor and accelerometer
+    // Make sure the motor is stopped and ready to go.
+   motor.setSpeed( 25 );
+   motor.run( RELEASE );
+
 }
 
 void loop() {
